415aed7e23dee119124fa9e1f8920806856da4df
Diana Stebbins <dianastebbins97@gmail.com>
2020-04-09 09:44:55 -0700

    delete a note works, created functions for adding/deleting from list

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,14 +7,69 @@ module.exports = function (app) {
     // an array to hold all the saved data locally
     const notesArr = [];
 
-    // GET: when user asks for the Notes list
+    // GET: 
     app.get("/api/notes", function (request, response) {
+        // retrieve list from memory and return fresh list to the page
+        retrieveNotes();
+        return response.json(notesArr);
+    });
+
+    // POST: adds new Note to the list
+    app.post("/api/notes", function (request, response) {
+        // create/save new Note and return the updated list to the page
+        saveNewNote(request.body);
+        return response.json(notesArr);
+    });
+
+    // DELETE: deletes a Note from the list
+    app.delete("/api/notes/:id", function (request, response) {
+        // delete Note designated by incoming id and return the updated list to the page
+        deleteNote(request.params.id);
+        return response.json(notesArr);
+    });
+
+    function saveNewNote(data) {
+        // use timestamp to create an id for the new note
+        const newNote = {
+            title: data.title,
+            text: data.text,
+            id: Date.now().toString()
+        }
+        notesArr.push(newNote);
+        console.log(`Added note id ${newNote.id}`);
+        persist();
+    }
+
+    function deleteNote(idToDelete) {
+        // find the existing note with the incoming note id
+        const indexToDelete = findNoteById(idToDelete);
+
+        // remove note from the list, and persist the edited list
+        if (indexToDelete > -1) {
+            notesArr.splice(indexToDelete, 1);
+            console.log(`Removed note id ${idToDelete}`);
+            persist();
+        }
+    }
+
+    function findNoteById(idToFind) {
+        for (let index = 0; index < notesArr.length; index++) {
+            const element = notesArr[index];
+            if (element.id === idToFind) {
+                return index;
+            }
+        }
+        // id not found
+        return -1;
+    }
+
+    function retrieveNotes() {
         fs.readFile("./db/db.json", "utf8", function (error, data) {
             if (error) {
                 return console.log(error);
             }
 
-            // for every element in db.json, create a note object and save it to notes Arr
+            // for every element in db.json, create a note object and save it to notesArr
             notesArr.length = 0;
             let tempArr = JSON.parse(data);
             tempArr.forEach(element => {
@@ -25,48 +80,15 @@ module.exports = function (app) {
                 }
                 notesArr.push(newNote);
             });
-
-            return response.json(notesArr);
         });
-    });
-
-    // POST: when user adds to the Notes list
-    app.post("/api/notes", function (request, response) {
-        // make a new note from incoming request.body data
-        const data = request.body;
-
-        // use timestamp to create an id for the new note
-        const newNote = {
-            title: data.title,
-            text: data.text,
-            id: Date.now().toString()
-        }
-
-        // add new Note to the list, and persist the new list
-        notesArr.push(newNote);
-        persist();
-
-        // return the updated list 
-        return response.json(notesArr);
-    });
+    }
 
     function persist() {
         fs.writeFile("./db/db.json", JSON.stringify(notesArr), function (err) {
             if (err) {
                 return console.log(err);
             }
-            console.log(`Saved`);
+            console.log(`Updated db.json`);
         });
     }
-
-    // DELETE: when user deletes from the Notes list
-    app.delete("api/notes:id", function (request, response) {
-        // find the existing note
-        var chosen = request.params.id;
-
-        console.log(chosen);
-        // remove it from the list
-        // return the updated list
-        return response.json(noteArr);
-    });
 }
