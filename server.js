2462d513b9430589ff899776f64c51e428c990a2
Diana Stebbins <dianastebbins97@gmail.com>
2020-04-08 23:14:17 -0700

    added line to use static public folder

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Setup Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("./public"));
 
 // Routing
 require("./routes/apiRoutes")(app);
